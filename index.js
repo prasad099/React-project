21b7c98cadd4b05e8fcece0fd386d912e4fe4105
prasad009 <prasadbacche01@gmail.com>
2024-05-10 19:00:44 +0530

    start with parcel in react project

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const h1 = document.createElement("h1");
-h1.innerText = "hello world";
-document.body.appendChild(h1);  
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 console.log("React", React);
-console.log("react dom",ReactDOM);
+console.log("ReactDOM", ReactDOM);
+
+const heading1 = React.createElement("h1",{className :"heading1"},"Kolkatta Knight Riders");
+const heading2 = React.createElement("h1",{className :"heading2"},"Mumbai Indians");
+
+const container1 = React.createElement("div",{className :"container"},heading1, heading2);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container1);
